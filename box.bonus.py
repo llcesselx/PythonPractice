5a67e0a5ed96d56c27c1ca995e7dc13b2a171a49
llcesselx <108751430+llcesselx@users.noreply.github.com>
2022-09-25 17:57:12 -0400

    Create box.bonus.py

---
+++ box.bonus.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+import sys
+
+args = sys.argv[1:]
+box = ""
+corner ="+"
+side = "|"
+top_bot = "-"
+
+version = "1.1.BONUS!"
+
+def help_menu():
+    print("\n███   ████▄    ▄     ▄      ▄▄▄▄▄   ")
+    print("█  █  █   █     █     █    █     ▀▄ ")
+    print("█ ▀ ▄ █   █ ██   █ █   █ ▄  ▀▀▀▀▄   ")
+    print("█  ▄▀ ▀████ █ █  █ █   █  ▀▄▄▄▄▀    ")
+    print("███         █  █ █ █▄ ▄█            ")
+    print("            █   ██  ▀▀▀             ")
+    print("                                   \n")
+    print("               Commands:\n")
+    print("-v  --version  --------------  Version Number\n")
+    print("-h  --help  -----------------  Help Menu\n")
+    print("-H  --horizontal  -----------  Set top and bottom character")
+    print('                               i.e. -H "-" will set top')
+    print("                               and bottom borders to -\n")
+    print("-V  --vertical  -------------  Set side border character")
+    print('                               i.e. -V "|" will set sides')
+    print("                               to | character\n")
+    print("-C  --corner  ---------------  Set the corner characters")
+    print('                               i.e. -C "+" will set corners')
+    print("                               to + character")
+    print("               Directions:\n")
+    print("This program will create a box. You may enter")
+    print("your preferred dimensions in the arguments...")
+    print("e.g. box.py 10 5")
+    print("This will create a box with a WIDTH of the")
+    print("first number and a HEIGHT of the second number.\n")
+    print("Our example above would create a box like this:\n")
+    print("+--------+     You can see the 10 characters at")
+    print("|        |     the top and the bottom of the box")
+    print("|        |     because we specified a column width")
+    print("|        |     of 10. We will also see 5 characters")
+    print("+--------+     going up and down because we specified")
+    print("               a row height of 5.")
+
+if "-v" in args or "--version" in args:
+    print(f"VERSION: {version}")
+    exit(0)
+
+if "-h" in args or "--help" in args:
+    help_menu()
+    exit(0)
+
+if "-C" in args or "--corner" in args:
+    if "-C" in args:
+        c_arg = args.index("-C")
+    else:
+        c_arg = args.index("--corner")
+
+    print(c_arg)
+
+    try:
+        corner = args[c_arg+1]
+    except IndexError:
+        corner = "+"
+
+if "-H" in args or "--horizontal" in args:
+    if "-H" in args:
+        h_arg = args.index("-H")
+    else:
+        h_arg = args.index("--horizontal")
+
+    try:
+        top_bot = args[h_arg+1]
+    except IndexError:
+        top_bot = "-"
+
+if "-V" in args or "--vertical" in args:
+    if "-V" in args:
+        v_arg = args.index("-V")
+    else:
+        v_arg = args.index("--vertical")
+
+    try:
+        side = args[v_arg+1]
+    except IndexError:
+        side = "-"
+
+if len(args) == 0:
+    args.insert(0,8)
+    args.insert(1,8)
+
+try:
+    cols = int(args[0])
+except ValueError:
+    msg = "Need to enter integer, Columns (Width) set at 8"
+    cols = 8
+    print(msg, file=sys.stderr)
+    pass
+
+try:
+    rows = int(args[1])
+except ValueError:
+    msg = "Need to enter integer, Rows (Height) set at 8"
+    rows = 8
+    print(msg, file=sys.stderr)
+    pass
+
+for y in range(rows):
+    for x in range(cols):
+        if (x == 0 and y == 0) or (x == 0 and y == rows-1):
+            box+=(corner)
+        elif (x ==cols-1 and y == 0) or (x == cols-1 and y == rows-1):
+            box+= (corner+"\n")
+        elif (x > 0 and y == 0) or (x > 0 and y == rows-1):
+            box += (top_bot)
+        elif (x == 0 and y > 0):
+            box += (side)
+        elif  (x == cols-1 and y > 0):
+            box += (side+"\n")
+        else:
+            box += (" ")
+
+print(box)
+print(f"COLUMNS: {cols}\nROWS: {rows}")
