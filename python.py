ecc51e8832e37ed7a6047171549b3f5ceecc40a5
llcesselx <108751430+llcesselx@users.noreply.github.com>
2023-06-24 12:51:22 -0400

    can enter any value and it will show a table of corresponding values

--- python.py
+++ python.py
@@ -1,3 +1,19 @@
-import sys
-import random 
-import datetime
+def main():
+    num = int(input("Enter starting value: "))
+    _num = num
+    maxnum = num*5
+    i = 0
+
+    print("A\t\tA+2\t\tA+4\t\tA+6")
+    print("------------------------------")
+    while i < maxnum:
+        # print(i)
+        print("{}\t\t{}\t\t{}\t\t{}".format(num, num+2, num+4, num+6))
+        i += _num
+        num += _num
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
